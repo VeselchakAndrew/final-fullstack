2214f8c711dbe50faf6114a7c15cb7c63ab0eee2
Andrew Fedina <veselchak@gmail.com>
2021-12-26 22:49:57 +0200

    Fixed checker module

--- src/main/java/com/example/springtest/SpringTestApplication.java
+++ src/main/java/com/example/springtest/SpringTestApplication.java
@@ -1,4 +1,4 @@
-package com.example.spring_test;
+package com.example.springtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringTestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringTestApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SpringTestApplication.class, args);
+  }
 
 }
--- src/test/java/com/example/springtest/SpringTestApplicationTests.java
+++ src/test/java/com/example/springtest/SpringTestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.spring_test;
+package com.example.springtest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
