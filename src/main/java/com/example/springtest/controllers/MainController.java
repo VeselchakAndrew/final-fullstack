e95fbce53f2d67e810ae4342da54f931ca9b4b51
Andrew Fedina <veselchak@gmail.com>
2022-01-03 14:45:53 +0200

    Test commit

--- src/main/java/com/example/springtest/controllers/MainController.java
+++ src/main/java/com/example/springtest/controllers/MainController.java
@@ -1,2 +1,8 @@
-package com.example.springtest.controllers;public class MainController {
+package com.example.springtest.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
 }
