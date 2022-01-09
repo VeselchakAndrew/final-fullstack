f39a00f57f3dc6b8667e83414fb3bd3a316b6825
Andrew Fedina <veselchak@gmail.com>
2022-01-09 17:33:22 +0200

    Add test

---
+++ src/main/java/com/example/springtest/UserService.java
@@ -0,0 +1,13 @@
+package com.example.springtest;
+
+import com.example.springtest.DTO.User;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+  List<User> users = new ArrayList<>();
+
+  public List<User> getAllUsers() {
+    return users;
+  }
+}
